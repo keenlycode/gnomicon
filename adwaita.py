3e4a6944d234bb7e8bceb097de84581f7168d7fa
Nitipit Nontasuwan <nitipit@gmail.com>
2018-10-04 03:00:21 +0700

    update

--- adwaita.py
+++ adwaita.py
@@ -8,18 +8,35 @@ for w in walk:
         icon = os.path.join(w[0],icon)
         icons.append(icon)
 
-svg_def = ElementTree.fromstring('<svg></svg>')
+f = open('adwaita.svg', 'w')
+f.write('<svg></svg>')
+f.close()
+ElementTree.register_namespace('', 'http://www.w3.org/2000/svg')
+svg_def = ElementTree.parse('adwaita.svg')
 for icon in icons:
+    print(icon)
     f = open(icon)
     icon = f.read()
     f.close()
     svg = ElementTree.fromstring(icon)
     svg = svg.getchildren()[0]
+    try:
+        del(svg.attrib['color'])
+    except:
+        pass
     symbol = ElementTree.fromstring('<symbol></symbol>')
     symbol.set('viewbox', '0 0 16 16')
-    symbol.set('id', f.name)
+    symbol.set('id', f.name.split('/')[-1].replace('.svg', ''))
     symbol.append(svg)
-    svg_def.append(symbol)
+    svg_def.getroot().append(symbol)
 
-print(ElementTree.tostring(symbol))
-# print(svg_def.getchildren())
+svg_def.write('adwaita.svg')
+
+f = open('adwaita.svg', 'r')
+svg = f.read()
+f.close()
+svg = svg.replace('fill="#474747"', 'fill="currentColor"')
+svg = svg.replace('stroke="#474747"', 'stroke="currentColor"')
+f = open('adwaita.svg', 'w')
+f.write(svg)
+f.close()

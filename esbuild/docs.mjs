e442ca6e2f8cc532d7ea2fcf14339149b77ba464
Nitipit Nontasuwan <nitipit@gmail.com>
2023-12-11 23:57:49 +0700

    update

---
+++ esbuild/docs.mjs
@@ -0,0 +1,35 @@
+import * as esbuild from 'esbuild'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { glob } from 'glob';
+
+
+const __file = fileURLToPath(import.meta.url);
+const __dir = path.dirname(__file);
+
+const docs_src_dir = path.join(__dir, '../docs-src/');
+const docs_dir = path.join(__dir, '../docs/');
+
+let files = path.join(docs_src_dir, '**/*.{js,ts}');
+let ignores = [
+    path.join(docs_src_dir, '**/_*.{js,ts}')
+];
+
+files = await glob(files, {ignore: ignores});
+console.log(files);
+
+let server = await esbuild.context({
+    entryPoints: files,
+    outdir: docs_dir,
+    bundle: true
+})
+  
+let { host, port } = await server.serve({
+    servedir: 'docs',
+});
+
+console.log(`esbuild server: http://${host}:${port}`);
+
+await server.watch();
+
+console.log('esbuild watching...');
